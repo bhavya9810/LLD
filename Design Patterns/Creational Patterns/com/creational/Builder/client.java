9f078f15d32c8f7dc2676a2f44456168c9c41393
bhavya singh <samgta9810@gmail.com>
2025-11-14 02:28:54 +0530

    Builder design pattern added

---
+++ Design Patterns/Creational Patterns/com/creational/Builder/client.java
@@ -0,0 +1,13 @@
+package com.creational.Builder;
+
+public class client {
+
+    public static void main(String[] args) {
+        User user = new User.UserBuilder()
+                .setUserName("Bhavya Singh")
+                .setUserId(007)
+                .build();
+
+        System.out.println(user.toString());
+    }
+}
